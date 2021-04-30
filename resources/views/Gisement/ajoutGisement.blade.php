7ed627a54baf7539a9bd3a6f7f365861b2b6a80f
Chad Gauthier <chad.gauthier32@gmail.com>
2021-04-30 14:38:05 -0400

    page d'ajout de gisements

--- resources/views/Gisement/ajoutGisement.blade.php
+++ resources/views/Gisement/ajoutGisement.blade.php
@@ -1,5 +1,28 @@
 @extends('template')
 
 @section('contenu')
-
+<div class="container">
+    <form action="" method="POST" enctype="multipart/form-data" class="row justify-content-md-center">
+        @csrf
+        <div class="form-group col-md-4">
+            <label for="pays">Pays</label>
+            <input type="text" class="form-control" name="pays" required>
+        </div>
+        <div class="form-group col-md-4">
+            <label for="capaciteEst">Capacité estimée</label>
+            <input type="number" class="form-control" name="capaciteEst" required>
+        </div>
+        <div class="form-group col-md-4">
+            <label for="capaciteConf">Capacité confirmée</label>
+            <input type="number" class="form-control" name="capaciteConf" required>
+        </div>
+        <div class="form-group col-md-4">
+            <label for="composition">Composition</label>
+            <input type="text" class="form-control" name="composition" required>
+        </div>
+        <div class="form-group col-md-4">
+            <button type="submit" class="btn btn-primary">Ajouter</button>
+        </div>
+    </form>
+</div>
 @endsection
